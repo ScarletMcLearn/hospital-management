72b96ddf0fe9aabc865bd3d37191aee6e3f422ff
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:04:39 +0600

    Update diseases.js

--- routes/diseases.js
+++ routes/diseases.js
@@ -1,8 +1,4 @@
-/*
-    GET /app/getdiseases -> return JSON with all diseases in the system
-    POST /app/adddisease -> add a new disease in the system
-    POST /app/deletedisease -> delete a disease from the system
-*/
+
 
 const express = require('express');
 const _ = require('lodash');
@@ -12,37 +8,11 @@ const router = express.Router();
 var {scoreOfDisease, Disease} = require('./../server/models/diseases.js');
 var {Patient} = require('./../server/models/patient.js');
 
-/*
-    GET /app/getdiseases -> return JSON with all diseases in the system, sorted alphabetically
-*/
-router.get('/app/getdiseases', (req, res) => {
-    Disease.find({}, null, {sort: {name: 1}}).then((diseases) => {
-        // Facade pattern -> make a simple JSON object, containing just the diseases names and scoreOfDisease
-        //                -> to easily communicate with the frontend
-        var scoreOfDiseaseJSON = {};
-
-        if (_.isArray(diseases)) {
-            for (var i = 0; i < diseases.length; ++i) {
-                scoreOfDiseaseJSON[diseases[i].name] = diseases[i].score;
-            }
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send(JSON.stringify(scoreOfDiseaseJSON));
-    }).catch((err) => {
-        console.log(err);
-        res.status(404).send();
-    });
-});
-
-/*
-    POST /app/adddisease -> add a new disease in the system
-*/
 router.post('/app/adddisease', (req, res) => {
     var diseaseName = req.body.diseaseName;
     var diseaseScore = req.body.diseaseScore;
 
-    // check that the name is a String and score is a Number
+
     if (_.isString(diseaseName) && !_.isNaN(diseaseScore)) {
         var disease = Disease({
             name: _.capitalize(diseaseName),
@@ -61,15 +31,12 @@ router.post('/app/adddisease', (req, res) => {
     }
 });
 
-/*
-    POST /app/deletedisease -> delete a disease from the system
-*/
 router.post('/app/deletediseases', (req, res) => {
     var diseasesToDelete = req.body.DD;
 
     if (_.isArray(diseasesToDelete)) {
         for (var i = 0; i < diseasesToDelete.length; ++i) {
-            // 1. Delete the disease from the system
+            
             var disease = diseasesToDelete[i];
             Disease.find({
                 name: diseasesToDelete[i]
@@ -82,7 +49,7 @@ router.post('/app/deletediseases', (req, res) => {
                  reject(disease);
             });
 
-            // 2. Update all patients
+            
             Promise.all([promise.then(function (disease) { return disease; }), Patient.find({ diseases: disease })])
                  .then((data) => {
                      var diseaseToDel = data[0];
@@ -93,7 +60,6 @@ router.post('/app/deletediseases', (req, res) => {
                           var patient = patients[i];
                           var newDiseases = [];
 
-                          // delete the diseases from the patient diseases array
                           for (var j = 0; j < patient.diseases.length; ++j)
                               if (patient.diseases[j] !== diseaseToDel) {
                                    newDiseases.push(patient.diseases[j]);
@@ -117,6 +83,25 @@ router.post('/app/deletediseases', (req, res) => {
         console.log("POST /app/deletedisease, diseasesToDelete is not an array");
         res.status(400).redirect('/app/systemsettings');
     }
+});
+
+router.get('/app/getdiseases', (req, res) => {
+    Disease.find({}, null, {sort: {name: 1}}).then((diseases) => {
+       
+        var scoreOfDiseaseJSON = {};
+
+        if (_.isArray(diseases)) {
+            for (var i = 0; i < diseases.length; ++i) {
+                scoreOfDiseaseJSON[diseases[i].name] = diseases[i].score;
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(scoreOfDiseaseJSON));
+    }).catch((err) => {
+        console.log(err);
+        res.status(404).send();
+    });
 });
 
 module.exports = router;
