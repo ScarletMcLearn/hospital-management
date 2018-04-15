4038d53f6fae28cebf6c43106490c8e2147b214d
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:11:49 +0600

    Update diseases.js

--- server/models/diseases.js
+++ server/models/diseases.js
@@ -1,6 +1,4 @@
-/*
-	mongoDB Schema for diseases
-*/
+
 const mongoose = require ('mongoose');
 
 var DiseaseSchema = mongoose.Schema({
@@ -17,12 +15,6 @@ var DiseaseSchema = mongoose.Schema({
 });
 
 var Disease = mongoose.model('Disease', DiseaseSchema);
-
-/*
- 	Default diseases in the system
-		-> those will be added as soon as the system is live
-		-> if they are deleted from the system, and the system restarts, then they will be added again in the system
-*/
 
 var scoreOfDisease = {}; // empty map
 
