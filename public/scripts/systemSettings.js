73d545dca63a93ec8bb5223964a2523faaaf81d6
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:02:56 +0600

    Update systemSettings.js

--- public/scripts/systemSettings.js
+++ public/scripts/systemSettings.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
            for(var disease in allDiseases) {
              var diseaseScoreCheckbox = [];
         	   diseaseScoreCheckbox[0] = disease;
-        	   diseaseScoreCheckbox[1] = allDiseases[disease]; // This is the score.
+        	   diseaseScoreCheckbox[1] = allDiseases[disease]; 
              diseaseScoreCheckbox[2] = "<input type=\"checkbox\" name=\"DD[]\" value=\"" + disease + "\">";
 
         	   diseasesScoresCheckboxes.push(diseaseScoreCheckbox)
@@ -40,7 +40,7 @@ $(document).ready(function() {
 });
 
 
-// fill the rooms table
+
 $(document).ready(function() {
     var roomsAPI = URL + "/app/getrooms/";
 
@@ -78,13 +78,3 @@ $(document).ready(function() {
     });
 });
 
-/*
-     Google analytics
-*/
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-97568701-1', 'auto');
-ga('send', 'pageview');
