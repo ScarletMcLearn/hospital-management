baf98b7c2449fe30de9b4628bb71b6c7b299163c
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:10:33 +0600

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,6 @@ var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//change the database with yours
 mongoose.connect('mongodb://localhost/advisorDemoTestDB', { useMongoClient: true });
-//mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
 
 module.exports = {mongoose};
