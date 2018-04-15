e6f6491d5e661f571993b77d2405d34a6bd4408d
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:09:53 +0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,3 @@
-/*
- 	POST /app/adduser -> add a new user in the system
-     GET /logout       -> log the user out of the system
-*/
 
 const express = require('express');
 const router = express.Router();
@@ -9,23 +5,17 @@ const passport = require ('passport');
 const LocalStrategy = require ('passport-local').Strategy;
 const User = require('./../server/models/user.js');
 
-/*
-    POST /app/adduser ->  add a new user in the system
-*/
 router.post('/app/adduser', (req, res) => {
 	var username = req.body.username;
 	var password = req.body.password;
 
-    // validation
 	req.checkBody('username', 'Username is required').notEmpty();
 	req.checkBody('password', 'Password is required').notEmpty();
 
-    // if there are errors, flash messages on the screen
     var errors = req.validationErrors();
     if(errors) {
         res.status(400).redirect('systemsettings');
     } else {
-        // if everything is OK, create a new user in the database
         var newUser = new User({
             username,
             password
@@ -43,9 +33,6 @@ router.post('/app/adduser', (req, res) => {
     }
 });
 
-/*
-	GET /app/logout -> function to logout from the system
-*/
 router.get('/app/logout', function(req, res) {
     req.logout();
     req.flash('success_msg', 'You are logged out');
