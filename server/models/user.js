c188705863f6203df541060e0c01b45a7057c351
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:12:41 +0600

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,4 @@
-/*
- 	Define the authentification system and the user model.
 
-	***Password are stored encrypted using Bcrypt algorithm.
-*/
 const mongoose = require ('mongoose');
 const bcrypt = require ('bcryptjs');
 
@@ -18,45 +14,36 @@ var UserSchema = mongoose.Schema({
 	},
 });
 
-// define the model User to be added in the database
 var User = module.exports = mongoose.model('User', UserSchema);
 
-/*
-	CREATE ADMIN ACCOUNT
-*/
 var adminUser = new User({
 	username: 'admin',
 	password: 'admin'
 });
 createUser(adminUser, function (aux1, aux2) {
-	// do nothing
+
 });
 
-
-/*
-	Create new User in the system
-*/
-// hash the password when creating a new user in the database
 function createUser(newUser, callback) {
 	bcrypt.genSalt(10, function(err, salt) {
 		  bcrypt.hash(newUser.password, salt, function(err, hash) {
-			  // store hash
+			 
 			  newUser.password = hash;
-			  newUser.save(callback); // the callback of save is a function(err, user)
+			  newUser.save(callback); 
 	  	  });
 	});
 }
 module.exports.createUser = createUser;
 
+module.exports.getUserById = function(id, callback) {
+	User.findById(id, callback);
+}
 module.exports.getUserByUsername = function(username, callback) {
 	User.findOne({
 		username: username
 	}, callback);
 }
 
-module.exports.getUserById = function(id, callback) {
-	User.findById(id, callback);
-}
 
 module.exports.comparePassword = function(candidatePassword, hash, callback) {
 	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
