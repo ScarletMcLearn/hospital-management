d1d27c52193d30e665d131c9843131fdd89dedba
saziasharmin <35674364+saziasharmin@users.noreply.github.com>
2018-04-15 18:05:28 +0600

    Update patients.js

--- routes/patients.js
+++ routes/patients.js
@@ -1,12 +1,4 @@
-/*
-     GET /app/addpatient -> go to addPatient page
-     POST /app/addpatient                -> add a patient in the database
-     GET  /app/getpatients               -> get a JSON with all patients
-     GET  /app/patient/:hospitalNumber   -> get one patiente data
-     GET  /app/getpatient/:hospitalNumber-> get JSON of a patiente data
-     POST /app/updatepatient/:hospitalNumber -> update disease & score for patient
-     POST /app/delete/:hospitalNumber -> detele a patient from the system
-*/
+
 
 const express = require('express');
 const _ = require('lodash');
@@ -18,30 +10,20 @@ var {rooms, Room} = require('./../server/models/rooms.js');
 var isValidDate = require('is-valid-date');
 const {ObjectID} = require('mongodb');
 
-
-/*
-    GET /app/addpatient -> go to addPatient page
-*/
 router.get('/app/addpatient', (req, res) => {
     res.render('addpatient', {pageTitle: "Add patient"});
 });
 
-/*
-    POST /addPatient -> add new patient
-*/
 router.post('/app/addpatient', (req, res) => {
-    // receive the diseases from the form in the array PD, each element being a String with the disease name
+   
     var PD = req.body.PD;
     var dateOfBirth = req.body.dateOfBirth;
 
-    // console.log(dateOfBirth);
-    // console.log(isValidDate(dateOfBirth));
 
-    if (_.isEmpty(PD)) {    // check if no disease is selected
+    if (_.isEmpty(PD)) {   
         PD = [];
     }
 
-    // Check for empty fields
     if (_.isEmpty(req.body.firstName) || _.isEmpty(req.body.lastName) || _.isEmpty(req.body.hospitalNumber) || !isValidDate(dateOfBirth)) {
         if (_.isEmpty(req.body.firstName)) req.flash('error_msg', 'Please enter the first name.');
         if (_.isEmpty(req.body.lastName)) req.flash('error_msg', 'Please enter the last name.');
@@ -49,8 +31,9 @@ router.post('/app/addpatient', (req, res) => {
         if (!isValidDate(dateOfBirth)) req.flash('error_msg', 'The date is not valid.');
 
         res.status(400).redirect('/app/addpatient');
-    } else {
-        // set the sex of the new patient
+    } 
+    else {
+        
         var sex = req.body.sex;
         if (sex === "male") {
             sex = true;
@@ -58,7 +41,6 @@ router.post('/app/addpatient', (req, res) => {
             sex = false;
         }
 
-        // make a new patient and add it in the database
         var patient = Patient({
             firstName: _.capitalize(req.body.firstName),
             lastName: _.capitalize(req.body.lastName),
@@ -79,21 +61,7 @@ router.post('/app/addpatient', (req, res) => {
    }
 });
 
-/*
-    GET /app/getpatients  -> get a JSON with all patients
-*/
-router.get('/app/getpatients', (req, res) => {
-    Patient.find({}).then((patients) => {
-        res.status(200).send(patients);
-    }).catch((err) => {
-        console.log(err);
-        res.status(400).send();
-    });
-});
 
-/*
-    GET one patient data -> for his personal page
-*/
 router.get('/app/patient/:hospitalNumber', (req, res) => {
     hospitalNumber = req.params.hospitalNumber;
     Patient.findOne({
@@ -109,9 +77,6 @@ router.get('/app/patient/:hospitalNumber', (req, res) => {
     });
 });
 
-/*
-    GET one patient data and return it as JSON
-*/
 router.get('/app/getpatient/:hospitalNumber', (req, res) => {
     hospitalNumber = req.params.hospitalNumber;
     Patient.findOne({
@@ -124,14 +89,20 @@ router.get('/app/getpatient/:hospitalNumber', (req, res) => {
     });
 });
 
-/*
-    POST /app/updatepatient/:hospitalNumber -> update disease & score for patient
-                                            -> request made from the patientPage
-*/
+
+router.get('/app/getpatients', (req, res) => {
+    Patient.find({}).then((patients) => {
+        res.status(200).send(patients);
+    }).catch((err) => {
+        console.log(err);
+        res.status(400).send();
+    });
+});
+
 router.post('/app/updatepatient/:hospitalNumber', (req, res) => {
     hospitalNumber = req.params.hospitalNumber;
 
-    // GET form attributes
+    
     var PD = req.body.PD;
     if (_.isEmpty(PD)) {
         PD = [];
@@ -154,10 +125,6 @@ router.post('/app/updatepatient/:hospitalNumber', (req, res) => {
         res.redirect('/app/patient/' + hospitalNumber);
     });
 });
-
-/*
-    POST /app/delete/:hospitalNumber -> detele a patient from the system
-*/
 router.get('/app/deletepatient/:hospitalNumber', (req, res) => {
     var hospitalNumber = req.params.hospitalNumber;
 
@@ -166,7 +133,6 @@ router.get('/app/deletepatient/:hospitalNumber', (req, res) => {
             var rooms = data[0];
             var patient = data[1];
 
-            // if the patient is in a room, make the room empty
             if (patient.room !== 'noroom') {
                  for (var i = 0; i < rooms.length; ++i) {
                     if (rooms[i].name === patient.room) {
